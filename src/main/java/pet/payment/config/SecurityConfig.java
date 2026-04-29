f5a44c0b514990b4eb831ec8fc7793ffc0b695f7
Ivanov Ilya <iiilya12@yandex.ru>
2026-04-29 22:26:54 +0300

    feat: add simple security config

---
+++ src/main/java/pet/payment/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package pet.payment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    public SecurityConfig() {
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+}
